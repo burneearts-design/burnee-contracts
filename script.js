cab47847029b9b704d4a56bc367a21020e35aded
burneearts-design <burneearts@gmail.com>
2026-03-22 15:24:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,30 @@
 const params = new URLSearchParams(window.location.search);
-let plan = params.get("plan");
-
-// default plan if none selected
-if (!plan) {
-plan = "standard";
-}
+let plan = params.get("plan") || "standard";
 
 const plans = {
 
-basic: {
-name: "Basic Plan",
-price: "$39 USD",
-deposit: "$20 USD",
-balance: "$19 USD",
-timeline: "3 Days"
+basic:{
+name:"Basic Plan",
+price:"$39 USD",
+deposit:"$20 USD",
+balance:"$19 USD",
+timeline:"3 Days"
 },
 
-standard: {
-name: "Standard Plan",
-price: "$78 USD",
-deposit: "$39 USD",
-balance: "$39 USD",
-timeline: "5 Days"
+standard:{
+name:"Standard Plan",
+price:"$78 USD",
+deposit:"$39 USD",
+balance:"$39 USD",
+timeline:"5 Days"
 },
 
-premium: {
-name: "Premium Plan",
-price: "$117 USD",
-deposit: "$59 USD",
-balance: "$58 USD",
-timeline: "7 Days"
+premium:{
+name:"Premium Plan",
+price:"$117 USD",
+deposit:"$59 USD",
+balance:"$58 USD",
+timeline:"7 Days"
 }
 
 };
@@ -43,8 +38,6 @@ document.getElementById("deposit").textContent = selected.deposit;
 document.getElementById("balance").textContent = selected.balance;
 document.getElementById("timeline").textContent = selected.timeline;
 
-
-// Accept contract
 function acceptContract(){
 
 const checkbox = document.getElementById("agreeTerms");
